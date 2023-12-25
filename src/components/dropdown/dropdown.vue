1aa91cbc340621d00fdb3fde8b7854d0501f7a2b
HHH1994 <15757135983@163.com>
2023-12-25 10:46:27 +0800

    fix: 优化dropdown交互

--- src/components/dropdown/dropdown.vue
+++ src/components/dropdown/dropdown.vue
@@ -1,51 +1,62 @@
 <template>
-  <div class="dropdown">
-      <div class="dropdown-trigger" @click="triggerHandle">
-            <slot>下拉菜单</slot>
-      </div>
-      <ul v-show="isShowPanel" ref="dropdownMenuWrapRef" class="dropdown-menu_wrap">
-          <li 
-            v-for="(command,index) of commands"
-            :key="index"
-            class="dropdown-menu"
-            @click="() => commandHandler(command)"
-          >{{command}}</li>
-      </ul>
+  <div ref="wrapper" class="dropdown">
+    <div class="dropdown-trigger" @click="triggerHandle">
+      <slot>下拉菜单</slot>
+    </div>
+    <ul ref="dropdownMenuWrapRef" class="dropdown-menu_wrap">
+      <li
+        v-for="(command,index) of commands"
+        :key="index"
+        class="dropdown-menu"
+        @click="() => commandHandler(command)"
+      >{{ command }}</li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
-   props: {
-       trigger: {
-           type: String,
-           default: 'click'
-       },
-       commands: {
-           type: Array,
-           default: () => []
-       }
-   },
-   data() {     
-     return {   
-       isShowPanel: false 
-     }          
-   },
-   methods: {
-       triggerHandle() {
-           if (this.isShowPanel) {
-               this.isShowPanel = false;
-               this.$refs.dropdownMenuWrapRef.style.maxHeight = '0'
-           } else {
-               this.isShowPanel = true;
-               this.$refs.dropdownMenuWrapRef.style.maxHeight = '100px'
-           }
-       },
-       commandHandler(val) {
-           this.$emit('command', val)
-           this.triggerHandle();
-       }
-   }        
-}
+  props: {
+    trigger: {
+      type: String,
+      default: 'click'
+    },
+    commands: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      isShowPanel: false
+    };
+  },
+  mounted() {
+    this.globalClickHandler = (e) => {
+      if (this.$refs.wrapper && this.$refs.wrapper.contains(e.target)) return;
+      this.isShowPanel = false;
+      this.$refs.dropdownMenuWrapRef.style.maxHeight = '0';
+    };
+    window.addEventListener('click', this.globalClickHandler);
+  },
+  beforeDestroy() {
+    window.removeEventListener('click', this.globalClickHandler);
+  },
+  methods: {
+    triggerHandle() {
+      if (this.isShowPanel) {
+        this.isShowPanel = false;
+        this.$refs.dropdownMenuWrapRef.style.maxHeight = '0';
+      } else {
+        this.isShowPanel = true;
+        this.$refs.dropdownMenuWrapRef.style.maxHeight = '100px';
+      }
+    },
+    commandHandler(val) {
+      this.$emit('command', val);
+      this.triggerHandle();
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
 .dropdown {
@@ -65,12 +76,12 @@ export default {
     min-width: 80px;
     max-height: 0;
     list-style: none;
-    padding: 5px 0;
     margin: 0;
     background: #fff;
     overflow-y: auto;
     box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
     z-index: 99;
+    transition: all 300ms;
 
     .dropdown-menu {
         padding: 5px 10px;
